5fdf3d17f840740ad8ccf871cabc0d06e9791bb1
Arik Rizki Akbar <arz147@upi.edu>
2023-05-23 22:38:19 +0700

    update framework

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,19 +1,8 @@
-// const Pool = require("pg").Pool;
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'be-sd-ciwaregu',
-//   password: 'arz140701',
-//   port: 5432 // the default PostgreSQL port
-// })
+import { Sequelize } from "sequelize";
 
-// module.exports = pool;
+const db = new Sequelize('be-sd-ciwaregu', 'postgres', 'arz140701', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
 
-// const Sequelize = require('sequelize');
-
-// const db = new Sequelize('be-sd-ciwaregu', 'postgres', 'arz140701', {
-//   host: 'localhost',
-//   dialect: 'postgres'
-// });
-
-// module.exports = db;
+export default db;
