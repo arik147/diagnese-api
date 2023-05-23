d54dc32be331b07be10401ff146ddd4a3d92b4ab
Arik Rizki Akbar <arz147@upi.edu>
2023-05-23 23:18:22 +0700

    update framework

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -5,4 +5,13 @@ const db = new Sequelize('be-sd-ciwaregu', 'postgres', 'arz140701', {
   dialect: 'postgres'
 });
 
+// Sinkron table pada db dari model
+/* db.sync({alter: true})
+  .then(() => {
+    console.log('Tabel berhasil di sinkronisasi');
+  })
+  .catch((error) => {
+    console.error('Terjadi kesalahan:', error);
+  }); */
+
 export default db;
